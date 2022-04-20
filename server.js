9111d0228922fe7e34aa874adcc8916906442448
chirag <chirag179652@gmail.com>
2022-04-20 23:31:21 +0530

    added body parser

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('<h1>This is the main route</h1>');
 });
@@ -8,6 +11,7 @@ app.get('/profile', (req, res) => {
   res.send('<h1>Getting Profile</h1>');
 });
 app.post('/profile', (req, res) => {
+  console.log(req.body);
   const user = {
     name: 'Amit',
     age: '24',
